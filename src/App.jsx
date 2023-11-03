79ea5a2d36d3700243fc6870e613ee1cdce24fca
Tienduc37y <laitienduc37@gmail.com>
2023-11-03 21:51:25 +0700

    config redux-toolkit and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
 import { useEffect,useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import {fetchDataFromApi} from './utils/api'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { getApiConfiguration } from './store/homeSlice'
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+
 function App() {
+  const dispatch = useDispatch()
+  const {url} = useSelector((state)=>state.home)
+  // console.log(url)
   const APITesting = () =>{
     fetchDataFromApi('/movie/popular')
     .then((res)=>{
         console.log(res)
+        dispatch(getApiConfiguration(res))
     })
   }
   useEffect(()=>{
     APITesting()
   },[])
   return (
-    <>
-      App
-    </>
+    <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
   )
 }
 
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.scss"
+import HeroBanner from './heroBanner/HeroBanner'
+
+const Home = () => {
+  return (
+    <div className='homePage'>
+      <HeroBanner></HeroBanner>
+    </div>
+  )
+}
+
+export default Home
