e4792d98f3b158c425886027297b300083513388
DESKTOP-VPCFAMG\ADMIN <nguyenvanmang2608@gmail.com>
2023-11-04 10:28:20 -0700

    HeroBanner Section CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useEffect,useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import {fetchDataFromApi} from './utils/api'
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
 
-import { useDispatch, useSelector } from 'react-redux'
-import { getApiConfiguration } from './store/homeSlice'
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -14,32 +14,31 @@ import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
-  const dispatch = useDispatch()
-  const {url} = useSelector((state)=>state.home)
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
   // console.log(url)
-  const APITesting = () =>{
-    fetchDataFromApi('/movie/popular')
-    .then((res)=>{
-        console.log(res)
-        dispatch(getApiConfiguration(res))
-    })
-  }
-  useEffect(()=>{
-    APITesting()
-  },[])
+  const APITesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
+  useEffect(() => {
+    APITesting();
+  }, []);
   return (
     <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/:mediaType/:id" element={<Details />} />
-                <Route path="/search/:query" element={<SearchResult />} />
-                <Route path="/explore/:mediaType" element={<Explore />} />
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-            <Footer />
-        </BrowserRouter>
-  )
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
