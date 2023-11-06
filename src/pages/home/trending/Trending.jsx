bbc69f6d914b0807124c1086c35af06d6b980356
DESKTOP-VPCFAMG\ADMIN <nguyenvanmang2608@gmail.com>
2023-11-06 18:31:18 +0700

    Main Carousel Component

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,27 +1,29 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
-import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
-import useFetch from '../../../hooks/useFetch';
+import Carousel from "../../../components/carousel/Carousel";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+
+import useFetch from "../../../hooks/useFetch";
 
 const Trending = () => {
+    const [endpoint, setEndpoint] = useState("day");
 
-    const [endpoint, setEndpoint] = useState("day")
-
-    const {data, loading} = useFetch(`/trending/all/${endpoint}`)
+    const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
 
     const onTabChange = (tab) => {
         setEndpoint(tab === "Day" ? "day" : "week");
     };
 
     return (
-        <div className='carouselSection'>
+        <div className="carouselSection">
             <ContentWrapper>
-                <span className='carouseTitle'>Trending</span>
-                <SwitchTabs data = {["day", "Week"]} onTabChange = {onTabChange} />
+                <span className="carouselTitle">Trending</span>
+                <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
             </ContentWrapper>
+            <Carousel data={data?.results} loading={loading} />
         </div>
     );
-}
+};
 
-export default Trending
+export default Trending;
