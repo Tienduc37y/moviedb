9512a6e77f99f1aa58bfbffeda042033ae71f04f
Tienduc37y <laitienduc37@gmail.com>
2023-11-10 00:03:53 +0700

    Details Page - Details Banner Section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import "./style.scss"
 
+import useFetch from '../../hooks/useFetch'
+import { useParams } from 'react-router-dom'
+import DetailsBanner from './detailsBanner/detailsBanner'
+
 const Details = () => {
+  const {mediaType, id} = useParams()
+  const {data, loading} = useFetch(`/${mediaType}/${id}/videos`)
+  const {data : credits, loading : creditsLoading } = useFetch(`/${mediaType}/${id}/credits`)
   return (
-    <div>Details</div>
+    <>
+      <DetailsBanner video={data?.results?.[0]} crew = {credits?.crew}></DetailsBanner>
+    </>
   )
 }
 
